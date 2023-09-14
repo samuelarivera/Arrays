e62078135b7839c19097c822711255081f064800
Samuel Rivera <samuel.rivera@mrjaffesclass.com>
2023-09-14 11:43:49 -0700

    steps one and two completed

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,15 +2,17 @@ public class HelloWorld
 {
     public static void main(String[] args) 
     {
-        String[][] stringarray = {
-            {"Alpha"},
-            {"Bravo"},
-            {"Charlie"},
-            {"Delta"},
-            {"Echo"}
-        };
-        stringarray.length = 4;
-
+        String[] stringarray = {"Alpha","Bravo","Charlie","Delta","Echo"};
+        for (int i = 0; i < 5; i = i++) {
+            System.out.println(stringarray[i]);
+        }
+        String[] stringar1 = {"aa","bb","cc","dd","ee"};
+        for (String p:stringarray) {
+            System.out.println(p);
+        }
+        }
+    
+    
     }
-}
+
 
